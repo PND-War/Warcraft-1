4c08f579af31fb0616c39233061960433d587248
Yaroslav <pitnichyk@gmail.com>
2021-07-26 16:10:19 -0400

    add methods

--- Warcraft 1/Scenes/Menu.cs
+++ Warcraft 1/Scenes/Menu.cs
@@ -1,7 +1,16 @@
-﻿namespace Warcraft_1.Scenes
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Warcraft_1.Scenes
 {
     class Menu : AScene
     {
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch) { }
+        public virtual void HandleInput(SpriteBatch spriteBatch, GameTime gameTime) { }
+        public virtual void Play(GameTime gameTime, SpriteBatch spriteBatch) { }
+        public virtual void Settings(GameTime gameTime, SpriteBatch spriteBatch) { }
 
     }
 }
