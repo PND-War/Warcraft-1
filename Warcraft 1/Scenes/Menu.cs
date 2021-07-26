ec827a5b6a3207df058cd4b6e04f10506c3e8326
Yaroslav <pitnichyk@gmail.com>
2021-07-26 16:14:22 -0400

    add class Settings and class Playing

--- Warcraft 1/Scenes/Menu.cs
+++ Warcraft 1/Scenes/Menu.cs
@@ -6,11 +6,7 @@ namespace Warcraft_1.Scenes
 {
     class Menu : AScene
     {
-
         public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch) { }
-        public virtual void HandleInput(SpriteBatch spriteBatch, GameTime gameTime) { }
-        public virtual void Play(GameTime gameTime, SpriteBatch spriteBatch) { }
-        public virtual void Settings(GameTime gameTime, SpriteBatch spriteBatch) { }
 
     }
 }
